1eb2b3a510ef64e36a9aece621fac4b49d737bf1
Amanda Lima <unexamanda@gmail.com>
2026-04-25 19:00:08 -0300

    Update sistema_vendas.sql

--- LojaCentral/sistema_vendas.sql
+++ LojaCentral/sistema_vendas.sql
@@ -13,4 +13,9 @@ valor_total numeric(10,2) not null,
 data_venda timestamp default current_timestamp
 );
 
+insert into produtos (nome, preco, estoque) values 
+('produto A', 10.00, 100),
+('produto B', 20.00, 50),
+('produto C', 5.00, 200);
 
+select * from produtos;
